fe55b8463487b70860991d5766c08107b84177e4
microchiip <GalinaAndrianova2013@yandex.ru>
2026-04-22 20:48:42 +0300

    :((((

--- src/main/ru/practicum/moviehub/model/Movie.java
+++ src/main/ru/practicum/moviehub/model/Movie.java
@@ -13,10 +13,27 @@ public class Movie {
         this.year = year;
     }
 
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
-    public String getTitle() { return title; }
-    public void setTitle(String title) { this.title = title; }
-    public int getYear() { return year; }
-    public void setYear(int year) { this.year = year; }
+    public int getId() {
+        return id;
+    }
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
 }
