a633573be165dd96ea9a79ec7fcee03ea7671e80
microchiip <GalinaAndrianova2013@yandex.ru>
2026-04-22 20:41:35 +0300

    :(

--- src/main/ru/practicum/moviehub/api/ErrorResponse.java
+++ src/main/ru/practicum/moviehub/api/ErrorResponse.java
@@ -16,6 +16,10 @@ public class ErrorResponse {
         this.details = details;
     }
 
-    public String getError() { return error; }
-    public List<String> getDetails() { return details; }
+    public String getError() {
+        return error;
+    }
+    public List<String> getDetails() {
+        return details;
+    }
 }
